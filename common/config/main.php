a04ede3b50797c74c34bef4887270863d82dd22d
unknown <hipogea@gmail.com>
2022-06-14 15:12:15 -0500

    sa

--- common/config/main.php
+++ common/config/main.php
@@ -101,6 +101,18 @@ return [
         // 'downloadAction' => 'gridview/export/download',
         // 'i18n' => []
         ],
+        'attachments' => [
+		'class' => nemmo\attachments\Module::className(),
+		'tempPath' => '@app/uploads/temp',
+		'storePath' => '@app/uploads/store',
+		'rules' => [ // Rules according to the FileValidator
+		    'maxFiles' => 10, // Allow to upload maximum 3 files, default to 3
+			'mimeTypes' => 'image/png', // Only png images
+			'maxSize' => 1024 * 1024 // 1 MB
+		],
+		'tableName' => '{{%attachments}}' // Optional, default to 'attach_file'
+	],
+        
     'admin' => [
         'class' => 'mdm\admin\Module',
      ], 
