65c6d6601ee9c4a3eda4ddd641058820733753b8
unknown <hipogea@gmail.com>
2022-07-07 17:02:13 -0500

    ew

--- frontend/modules/sunat/controllers/DefaultController.php
+++ frontend/modules/sunat/controllers/DefaultController.php
@@ -220,7 +220,7 @@ $util->showResponse($invoice, $cdr);
      
     if (isset($_POST['expandRowKey'])) {
         $model = SunatSends::findOne($_POST['expandRowKey']+0);
-          VAR_DUMP($model->id);
+          VAR_DUMP($model->id);die();
         if($model->resultado){
             //var_dump($model->mensaje);die();
             return $this->renderPartial('_send_result_success', ['model'=>$model,'cdr'=>$model->mensaje]);
