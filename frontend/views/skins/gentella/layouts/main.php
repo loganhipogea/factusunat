4d8f350f29f2bdcc6bb99dbd44cfe3a0a19aced1
unknown <hipogea@gmail.com>
2022-07-09 23:40:28 -0500

    wq

--- frontend/views/skins/gentella/layouts/main.php
+++ frontend/views/skins/gentella/layouts/main.php
@@ -265,9 +265,9 @@ echo ModalAjax::widget([
                             <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                                 <i class="fa fa-industry" style="font-size:1.5em;padding-right: 4px;color: #ffc24d;"></i>
                                 <?php 
-                                if(\yii::$app->sesion->get('permiso'))
+                                if(\yii::$app->session->get('permiso'))
                                 echo \common\models\masters\VwSociedades::despro();
-                                 \yii::$app->sesion->set('permiso',false);
+                                 \yii::$app->session->set('permiso',false);
                                 ?>
                                
                             </a>
