90c41910f01673080e6b5ed77bb3c98534afe836
unknown <hipogea@gmail.com>
2022-08-04 23:15:40 -0500

    ewew

--- frontend/views/masters/trabajadores/_traba_docu.php
+++ frontend/views/masters/trabajadores/_traba_docu.php
@@ -106,8 +106,9 @@ $this->params['breadcrumbs'][] = Yii::t('base.verbs', 'Assign');
            [
                'attribute'=>'codocu',
                'format'=>'raw',
-               'value'=>function($model){                                   
-                return Html::a("<span class='glyphicon glyphicon-download-alt'></span>".$model->documento->desdocu,$model->files[0]->url,['data-pjax'=>'0']);
+               'value'=>function($model){ 
+                  if($model->hasAttachments())
+                   return Html::a("<span class='glyphicon glyphicon-download-alt'></span>".$model->documento->desdocu,$model->files[0]->url,['data-pjax'=>'0']);
                }
            ],
             'numero',
