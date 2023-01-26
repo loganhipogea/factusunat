0175e75e6ce8dd0cd4da35dfeffb7231c819c3f3
unknown <hipogea@example.com>
2023-01-26 16:10:21 -0500

    wqwq

--- frontend/modules/com/models/ComDetcoti.php
+++ frontend/modules/com/models/ComDetcoti.php
@@ -223,6 +223,7 @@ class ComDetcoti extends \common\models\base\modelBase
   private function sincronizeMontos(){
       yii::error('sincornizando en detalles.. pasa a los padres refresh monto()',__FUNCTION__);
     yii::error($this->padre->refreshMontos()->save());
+    yii::error($this->padre->geterrors());
   }
   
   public function resolveScenario(){
