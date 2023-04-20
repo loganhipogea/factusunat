aab28da2e87798a2ad3241a9cf6130c36d613604
unknown <hipogea@gmail.com>
2023-04-20 16:30:43 -0500

    ass

--- frontend/modules/com/controllers/CotiController.php
+++ frontend/modules/com/controllers/CotiController.php
@@ -1208,8 +1208,10 @@ class CotiController extends baseController
        //echo $contenido; die();
         $pdf=$this->preparePdf($contenido);
         if(!$model->isAprobed()){
-                $pdf->SetWatermarkText('SIN APROBACION');
-                $pdf->showWatermarkText = true;
+               $pdf->SetWatermarkText('SIN APROBACION');
+                $pdf->showWatermarkText = true; 
+          }else{
+            $pdf->showWatermarkText = false;  
           }
       //  $pdf->WriteHTML($contenido);
         $pdf->Output($rutaTemporal .'/'. $nombre, \Mpdf\Output\Destination::INLINE);
