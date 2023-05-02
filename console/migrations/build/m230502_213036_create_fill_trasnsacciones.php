2be5ce9e634ea4b3aa8e600ce6f31afb613db6da
unknown <hipogea@gmail.com>
2023-05-02 18:46:51 -0500

    asasa

--- console/migrations/build/m230502_213036_create_fill_trasnsacciones.php
+++ console/migrations/build/m230502_213036_create_fill_trasnsacciones.php
@@ -8,12 +8,13 @@ use console\migrations\baseMigration;
 class m230502_213036_create_fill_trasnsacciones extends baseMigration
 {
  const NAME_TABLE='{{%transadocs}}';
+  const NAME_TABLE_DOCS='{{%documentos}}';
         
  //const NAME_TABLE_CENTROS='{{%centros}}';
     public function safeUp()
     {
        \Yii::$app->db->createCommand()->
-         batchInsert(static::NAME_TABLE,
+         batchInsert(static::NAME_TABLE_DOCS,
          ['codocu','desdocu','\frontend\modules\cc\models\CcCc'],
                 [['197','REGISTRO CECO','\frontend\modules\com\models\ComOv'],
                 ['198','ORDEN DE VENTA','\frontend\modules\com\models\ComCotizacion'],
@@ -31,39 +32,7 @@ class m230502_213036_create_fill_trasnsacciones extends baseMigration
              $this->fields(), $this->getData())->execute();
             
             
-            
-
-
-     ['codtrans','codocu','tipodoc','codestado'];
-       ['101','100','10','10'],
-['568','100','10','10'],
-['103','100','10','10'],
-['103','102','10','10'],
-['789','104','10','10'],
-['345','104','10','10'],
-['459','104','10','10'],
-['103','104','10','10'],
-['500','104','10','10'],
-['901','104','10','10'],
-['850','104','10','10'],
-['100','105','10','10'],
-['103','105','10','10'],
-['900','197','10','10'],
-['103','197','10','10'],
-['103','198','10','10'],
-['103','199','10','10'],
-['102','204','10','10'],
-['103','204','10','10'],
-['568','204','10','10'],
-['145','204','10','10'],
-['568','459','10','10'],
-['145','459','10','10'],
-['103','459','10','10'],
-['102','459','10','10'],
-['103','548','10','10'],
-['145','548','10','10'],
-['102','548','10','10'],      
-            
+       
     }
 
     public function safeDown()
@@ -119,6 +88,11 @@ class m230502_213036_create_fill_trasnsacciones extends baseMigration
     ->delete(self::NAME_TABLE)
     ->execute();
        
+        (new \yii\db\Query)
+    ->createCommand()
+    ->delete(self::NAME_TABLE)->Where(['codocu','in',['197','198','199','204','459','548']])
+    ->execute();  
+          
         }
     
 
