9f40fa3fcd7200f541fafed873f8773b465523cc
unknown <hipogea@gmail.com>
2023-05-02 22:07:16 -0500

    asasa

--- frontend/views/masters/materials/index.php
+++ frontend/views/masters/materials/index.php
@@ -28,9 +28,8 @@ $this->params['breadcrumbs'][] = $this->title;
              ['attribute' => 'descripcion',
                 'headerOptions' => ['style' => 'width:50%'],
                 'format'=>'raw',
-                'value'=>function($model){
-                            if($model->hasAttachments())
-                            return \yii\helpers\Html::img ($model->files[0]->url,['width'=>50,'height'=>50]);
+                'value'=>function($model){                            
+                            return $model->descripcion;
                             } 
                 
                 ],
